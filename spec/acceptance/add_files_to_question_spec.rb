55946ca587e177bcd44245d6ee3b67cdb0190aa8
aiidko <aiidko@mail.ru>
2017-01-22 15:30:04 +0500

    add attachments, link it with question

---
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -0,0 +1,24 @@
+require_relative 'acceptance_helper'
+
+feature 'add files to question', %q{
+  In order to illustrate my question
+  As an questions' author
+  I' like to be able to attach files
+} do
+  
+  given(:user) { create(:user) }
+
+  background do
+    sign_in(user)
+    visit new_question_path
+  end
+
+  scenario 'user adds file when asks question' do
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Text testx question'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Create'
+
+    expect(page).to have_content 'spec_helper.rb'    
+  end
+end
