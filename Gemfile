913c5eec22fcc7e456f115bc795ab7ce709dc8c0
aiidko <aiidko@mail.ru>
2017-02-11 00:06:30 +0500

    facebook authorization

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'simple_form'
 gem 'gon'
 gem 'skim'
 gem 'responders'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,32 @@ class User < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :votes, foreign_key: :users_id
   has_many :comments, foreign_key: :users_id
-  
+  has_many :authorizations
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   def author_of?(message)
     id == message.user_id
+  end
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[1, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+    user
+  end
+
+  def create_authorization(auth) 
+    authorizations.create(provider: auth.provider, uid: auth.uid) 
   end
 end
