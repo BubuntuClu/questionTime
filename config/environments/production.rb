0de4554dcf042202c817e146fb507682eb483420
aiidko <aiidko@mail.ru>
2017-03-12 19:17:53 +0500

    try to fix cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://77.244.213.178/cable'
-  config.action_cable.allowed_request_origins = [ 'http://host' ]
+  config.action_cable.allowed_request_origins = [ /http:\/\/*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
