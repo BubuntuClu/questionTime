e5804dd71d8ffdcfbe949c87745e17b2d431fe9d
aiidko <aiidko@mail.ru>
2017-03-13 19:14:39 +0500

    fix voting

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -6,14 +6,14 @@ $(document).on('turbolinks:load', function(){
     var result = $.parseJSON(xhr.responseText);
     $('#vote_' + result.id).html("<p>Rating: " + result.rating + "</p>");
     if (result.action == "vote"){
-      $('#' + result.id).parent().find('.vote_up').addClass('hidden')
-      $('#' + result.id).parent().find('.vote_down').addClass('hidden')
-      $('#' + result.id).parent().find('.unvote').removeClass('hidden')
+      $('#vote_' + result.id).parent().find('.vote_up').addClass('hidden')
+      $('#vote_' + result.id).parent().find('.vote_down').addClass('hidden')
+      $('#vote_' + result.id).parent().find('.unvote').removeClass('hidden')
     }
     if (result.action == "unvote"){
-      $('#' + result.id).parent().find('.vote_up').removeClass('hidden')
-      $('#' + result.id).parent().find('.vote_down').removeClass('hidden')
-      $('#' + result.id).parent().find('.unvote').addClass('hidden')
+      $('#vote_' + result.id).parent().find('.vote_up').removeClass('hidden')
+      $('#vote_' + result.id).parent().find('.vote_down').removeClass('hidden')
+      $('#vote_' + result.id).parent().find('.unvote').addClass('hidden')
     }
   }).bind('ajax:error', function(e, xhr, status, error){
     alert(2);
